1a9fb8b9ab86a89b370b6f6a473cfef90757bdbc
Stefen Dahlgren <31549513+Choadis@users.noreply.github.com>
2017-11-02 13:21:53 -0400

    no message

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-var mapOptions = {
-    center: new google.maps.LatLng(37.7831,-122.4039),
-    zoom: 12,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-};
 
-map = new google.maps.Map(document.getElementById('map'), mapOptions);
