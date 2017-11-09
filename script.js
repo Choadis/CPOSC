7cd0aaa4bfed341d575577030837deb8a6b2e6a0
Stefen Dahlgren <31549513+Choadis@users.noreply.github.com>
2017-11-09 14:37:07 -0500

    clickable button done, script location fixed on all pages

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+// Burger bar stuff
+var button = document.querySelector('.burger');
+var menu= document.querySelector('.ddmenu');
 
+button.addEventListener('click', function(){
+  menu.classList.toggle('none')
+});
