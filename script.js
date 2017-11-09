2d7ccc41ddb613c0a31d7a4a97e986174c835905
Stefen Dahlgren <31549513+Choadis@users.noreply.github.com>
2017-11-09 16:20:35 -0500

    no message

--- script.js
+++ script.js
@@ -3,5 +3,16 @@ var button = document.querySelector('.burger');
 var menu= document.querySelector('.ddmenu');
 
 button.addEventListener('click', function(){
-  menu.classList.toggle('none')
+  menu.classList.toggle('none');
 });
+
+// Collapsing carrots //
+var carrotButton = body.getElementsByClassName('gold-carrot');
+var none = body.getElementsByClassName('none');
+console.log(carrotButton);
+
+// carrotButton.addEventListener('click', function(){
+//     if (none.classList == 'none'){
+//     div.classList.toggle('none');
+//   }
+// });
