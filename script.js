79c3b1f09240ca7609a50805a6b1d49a579f3d2d
Stefen Dahlgren <31549513+Choadis@users.noreply.github.com>
2017-11-11 01:48:04 -0500

    got the ticket totaler working completely, along with the input box for the dono ticket

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var x = 0;
 
 function ticketCount(num, cls){
-  var totalBox = document.getElementsByClassName('totalBox');
+  var totalBox = document.getElementsByClassName('totalBox')[0];
   x = x + num;
   totalBox.innerHTML = "$" + x;
   document.getElementsByClassName(cls)[0].classList.toggle('none');
