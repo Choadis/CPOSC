caca74c9e186796569a82dcd56e63e40e7f44721
Stefen Dahlgren <31549513+Choadis@users.noreply.github.com>
2017-11-11 00:36:40 -0500

    got the ticket counter thingy mostly working, just cant get it to write to the total box.

--- script.js
+++ script.js
@@ -1,20 +1,8 @@
-function calcDate() {
-  var cposc = new Date('November, 1 2018');
-  var now = new Date();
-  var numberDays = ((cposc.getTime() / 1000 / 60 / 60 / 24) - (now.getTime() / 1000 / 60 / 60 / 24));
-  return Math.floor(numberDays);
+var x = 0;
+
+function ticketCount(num, cls){
+  var totalBox = document.getElementsByClassName('totalBox');
+  x = x + num;
+  totalBox.innerHTML = "$" + x;
+  document.getElementsByClassName(cls)[0].classList.toggle('none');
 }
-function countdownTimer(string) {
-  string = string + '';
-  string.split('')
-  var firstString = document.createTextNode(string[0]);
-  var firstBox = document.getElementsByClassName('count-box1')[0];
-  firstBox.appendChild(firstString);
-  var secondString = document.createTextNode(string[1]);
-  var secondBox = document.getElementsByClassName('count-box2')[0];
-  secondBox.appendChild(secondString);
-  var thirdString = document.createTextNode(string[2]);
-  var thirdBox = document.getElementsByClassName('count-box3')[0];
-  thirdBox.appendChild(thirdString);
-}
-countdownTimer(calcDate());
